1812fa5f403bf2a4d2397eb2e36e1fdfc66c792b
Markin Pavel <markinpashka@gmail.com>
2026-01-19 10:16:36 +0300

    Comments lesson 2_2

--- src/main/kotlin/lesson2_2.kt
+++ src/main/kotlin/lesson2_2.kt
@@ -18,11 +18,28 @@ fun main() {
     //Задача 3
     println("Задача 3")
     val serverResponse: Array<Any> = arrayOf("error", 400)
+    //при проверке as? не обязательно первый аргумет брать в круглые скобки, это только загромождает
+    //Если туже безопасно проверили через as? Int, то дальше уже лучше проверить на наличие значения через == null
+    // val systemResponseCode = serverResponse[1] as? Int
+    // if (systemResponseCode != null) {
+    //     println("Код ошибки: ${serverResponse[1]}")
+    //} else {
+    //    println("Неизвестный код ошибки")
+    //}
     if (((serverResponse[1]) as? Int) is Int) { println("Код ошибки: ${serverResponse[1]}")}
     else println("Неизвестный код ошибки")
 }
 
 //Задача 1
+//Мы еще не обговаривали, но лучше сразу привыкнуть к правильному форматированию if
+//Есть вариант записи в одну строку если она умещается в огороренную длину, тогда мы фигурные скобки не используем
+//if (searchId > 0) return "Элемент №$searchId" else return null
+// Второй вариант классический, каждый блок выделяется фигурными скобками и с новой строки
+//if (searchId > 0) {
+//    return "Элемент №$searchId"
+//} else {
+//    return null
+//}
 fun simulateElementSearch(searchId:Int) :String? {
     if (searchId > 0) {return "Элемент №$searchId"}
     else return null
