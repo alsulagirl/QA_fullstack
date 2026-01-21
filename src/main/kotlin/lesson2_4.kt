f9e377274254cb927146c4392a04ed899bf37293
Alsou Shaydullina <35466659+alsulagirl@users.noreply.github.com>
2026-01-21 14:46:17 +0300

    Update lesson2_4.kt

--- src/main/kotlin/lesson2_4.kt
+++ src/main/kotlin/lesson2_4.kt
@@ -4,9 +4,8 @@ fun main() {
     //Задача 3
     val actualCode = simulateApiRequest()
     checkStatus(200, actualCode)
-
-
 }
+
     //Задача 1
 fun checkStatus(expectedCode: Int, actualCode:Int) {
    println("Ожидаемый код: $expectedCode, Фактический код: $actualCode")
