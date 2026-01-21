72149f1ae5d8d5e6ade7c06024862f9393411ea4
ashaidullina <alsush4idullina@yandex.ru>
2026-01-21 14:02:25 +0300

    correct result lesson2_2

--- src/main/kotlin/lesson2_2.kt
+++ src/main/kotlin/lesson2_2.kt
@@ -3,15 +3,15 @@ package org.example
 //Задача 2
 fun main() {
 
-    val checkSearch : Array<Int> = arrayOf(-1,0,1)
+    val checkSearch: Array<Int> = arrayOf(-1, 0, 1)
     println("Задача 1+2")
-    val resultSimulateElementSearch1 : String = simulateElementSearch(checkSearch[0]) ?: "Элемент не найден"
+    val resultSimulateElementSearch1: String = simulateElementSearch(checkSearch[0]) ?: "Элемент не найден"
     println("Для ${checkSearch[0]} $resultSimulateElementSearch1")
 
-    val resultSimulateElementSearch2 : String = simulateElementSearch(checkSearch[1]) ?: "Элемент не найден"
+    val resultSimulateElementSearch2: String = simulateElementSearch(checkSearch[1]) ?: "Элемент не найден"
     println("Для ${checkSearch[1]} $resultSimulateElementSearch2")
 
-    val resultSimulateElementSearch3 : String = simulateElementSearch(checkSearch[2]) ?: "Элемент не найден"
+    val resultSimulateElementSearch3: String = simulateElementSearch(checkSearch[2]) ?: "Элемент не найден"
     println("Для ${checkSearch[2]} $resultSimulateElementSearch3")
 
 
@@ -26,8 +26,9 @@ fun main() {
     //} else {
     //    println("Неизвестный код ошибки")
     //}
-    if (((serverResponse[1]) as? Int) is Int) { println("Код ошибки: ${serverResponse[1]}")}
-    else println("Неизвестный код ошибки")
+    if ((serverResponse[1] as? Int) is Int) {
+        println("Код ошибки: ${serverResponse[1]}")
+    } else println("Неизвестный код ошибки")
 }
 
 //Задача 1
@@ -40,8 +41,11 @@ fun main() {
 //} else {
 //    return null
 //}
-fun simulateElementSearch(searchId:Int) :String? {
-    if (searchId > 0) {return "Элемент №$searchId"}
-    else return null
+fun simulateElementSearch(searchId: Int): String? {
+    if (searchId > 0) {
+        return "Элемент №$searchId"
+    } else {
+        return null
+    }
 }
 
