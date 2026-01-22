9b1a65259a2057eb0f296c8123d28ca601c239b4
ashaidullina <alsush4idullina@yandex.ru>
2026-01-22 17:10:02 +0300

    added result lesson2_5

---
+++ src/main/kotlin/lesson2_5.kt
@@ -0,0 +1,32 @@
+package org.example
+
+fun main() {
+    //Задача 3
+    val logTestStart : () -> Unit = {  println("Тест запущен. Время запуска теста: ${java.time.LocalTime.now()}") }
+
+    //Задача 1
+    val testNumber: Array<Int> = arrayOf(0, 1, -1)
+
+    val isPositive: (Int) -> Boolean = ({ number -> number > 0 })
+
+    for (currentNumberValue in testNumber) {
+        logTestStart()
+        println("Число $currentNumberValue - ${if (isPositive(currentNumberValue)) "положительное" else "неположительное"}")
+    }
+
+    //Задача 2
+    val isValidPhone = { testPhone: String ->
+        testPhone.contains("+7") && testPhone.substring(2)
+            .all { it.isDigit() } && (testPhone.count { it.isDigit() } == 11)
+    }
+
+    logTestStart()
+    val invalidPhone = "+700000000022"
+    println("$invalidPhone - ${if (isValidPhone(invalidPhone)) "V" else "X"}")
+
+    logTestStart()
+    val correctPhone = "+70000000002"
+    println("$correctPhone - ${if (isValidPhone(correctPhone)) "V" else "X"}")
+}
+
+
